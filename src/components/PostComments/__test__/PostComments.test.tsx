bdfae37bcc3835f2344ca9b331854df6f3aa5b0f
Kaio Felipe <kaiofelipee12@gmail.com>
2025-07-07 23:01:46 -0300

    update teste

--- src/components/PostComments/__test__/PostComments.test.tsx
+++ src/components/PostComments/__test__/PostComments.test.tsx
@@ -1,44 +1,27 @@
-// Importa as funções necessárias para realizar os testes
 import { fireEvent, render, screen } from '@testing-library/react';
 import PostComment from '..';
 
-// Conjunto de testes para o componente PostComment
 describe('Teste para o componente PostComment', () => {
 
-    // Teste para verificar se o componente é renderizado corretamente
     it('Deve renderizar o componente corretamente', () => {
-        // Renderiza o componente
         render(<PostComment/>);
-        // Verifica se o botão "Comentar" é mostrado após a renderização
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
 
-    // Teste para verificar se é possível adicionar dois comentários
     it('Deve adicionar dois comentários', () => {
-        // Renderiza o componente
         render(<PostComment/>);
-
-        // Adiciona o primeiro comentário
-        // Muda o valor da textarea para "Comentário adicionado via testes"
         fireEvent.change(screen.getByTestId('comment-textarea'), {
             target: {
                 value: 'Comentário adicionado via testes',
             }
         });
-        // Clica no botão para adicionar o comentário
         fireEvent.click(screen.getByTestId('comment-button'));
-
-        // Adiciona o segundo comentário
-        // Muda o valor da textarea para "Segundo comentário adicionado via testes"
         fireEvent.change(screen.getByTestId('comment-textarea'), {
             target: {
                 value: 'Segundo comentário adicionado via testes',
             }
         });
-        // Clica no botão para adicionar o comentário
         fireEvent.click(screen.getByTestId('comment-button'));
-
-        // Verifica se dois comentários foram adicionados
         expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
 });
--- src/components/PostComments/index.tsx
+++ src/components/PostComments/index.tsx
@@ -1,44 +1,31 @@
-// Importações dos hooks e estilos necessários
 import { FormEvent, useState } from 'react';
 import styles from './PostComments.module.css';
 
-// Importação do modelo de Comentário
 import Comment from '../../models/Comment';
 
 const PostComments = () => {
-    // Estado para armazenar os comentários
+
     const [comments, setComments] = useState<Comment[]>([]);
-    // Estado para armazenar o conteúdo temporário de um comentário
     const [tempComment, setTempComment] = useState('');
-
-    // Função para lidar com a adição de um novo comentário
     function handleAddComment(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault(); // Prevenir recarregamento da página
-
-        // Criar um novo comentário
+        event.preventDefault();
         const newComment = new Comment(comments.length, tempComment);
 
-        // Limpar o estado temporário do comentário
         setTempComment('');
-        // Atualizar a lista de comentários
         setComments([...comments, newComment]);
     }
 
     return (
         <div>
-            {/* Lista de comentários */}
             <ul className={styles['post-comments']}>
                 {comments.map(({ comment, id }) => (
                     <li data-testid="comment-element" className={styles['post-comment']} key={id}>
                         <p className={styles['post-comment-content']}>
-                            {/* Exibindo o conteúdo do comentário */}
                             {comment}
                         </p>
                     </li>
                 ))}
             </ul>
-
-            {/* Formulário para adição de novos comentários */}
             <form onSubmit={handleAddComment} className={styles['post-comments-form']}>
                 <textarea
                     data-testid="comment-textarea"
@@ -52,7 +39,6 @@ const PostComments = () => {
                     type="submit"
                     className={styles['post-comments-form-button']}
                 >
-                    {/* Botão para submeter o comentário */}
                     Comentar
                 </button>
             </form>
